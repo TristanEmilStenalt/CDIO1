1b2d452916d5d281a087ac34ac20364eee7b8f89
Tristan <tris2012@live.dk>
2019-02-26 12:15:38 +0100

    Gruppe 26. Skelet forket fra opgavebeskrivelse.

--- CDIO1/src/dal/IUserDAO.java
+++ CDIO1/src/dal/IUserDAO.java
@@ -10,7 +10,8 @@ public interface IUserDAO {
 	void createUser(UserDTO user) throws DALException;
 	void updateUser(UserDTO user) throws DALException;
 	void deleteUser(int userId) throws DALException;
-	
+
+	//Brugerdefineret Exception klasse, som extender Exception.
 	public class DALException extends Exception {
 
 		/**
